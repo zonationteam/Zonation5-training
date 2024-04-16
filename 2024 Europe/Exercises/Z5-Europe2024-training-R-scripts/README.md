3bd23417a0df6cbec7c72cb90a1f08bf5d93c01f
Ilkka Kivistö <ilkka.kivisto@helsinki.fi>
2024-04-16 14:38:14 +0300

    Create R project and feature file creation script

---
+++ 2024 Europe/Exercises/Z5-Europe2024-training-R-scripts/README.md
@@ -0,0 +1,7 @@
+# R scripts for Europe 2024 training data
+
+These files are not necessary for doing the exercises, but they showcase how you could store your Zonation 5 related data in CSV-format and how you can use R to create necessary files for running your Zonation 5 analyses.
+
+## Installation
+
+## Instructions
---
+++ 2024 Europe/Exercises/Z5-Europe2024-training-R-scripts/src/create-features-file-from-csv.R
@@ -0,0 +1,100 @@
+library(rstudioapi)
+library(dplyr)
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Functions ---------------------------------------------------------------
+
+create_features_file <- function(
+  features_file,
+  output_file,
+  columns,
+  all_selected_features=FALSE
+) {
+  features_df <- read.delim(features_file, sep=' ')
+  output_df <- features_df %>%
+    filter(SELECTED != '') %>%
+    arrange(SELECTED)
+  if (all_selected_features) {
+    write.table(
+      output_df %>% select(all_of(columns)),
+      output_file,
+      row.names=FALSE
+    )
+    return (0)
+  }
+  write.table(
+    subset(
+      output_df %>% filter(SELECTED == '1'),
+      select=columns
+    ),
+    output_file,
+    row.names=FALSE
+  )
+  return (0)
+}
+
+
+# Main script -------------------------------------------------------------
+
+# Exercise 1
+create_features_file(
+  features_file='../data/current_sdm_main.csv',
+  output_file='../../01_caz2/features.txt',
+  columns=c('filename'),
+  all_selected_features=FALSE
+)
+
+# Exercise 2
+create_features_file(
+  features_file='../data/current_sdm_main.csv',
+  output_file='../../02_cazmax/features.txt',
+  columns=c('filename'),
+  all_selected_features=FALSE
+)
+
+# Exercise 3
+create_features_file(
+  features_file='../data/current_sdm_main.csv',
+  output_file='../../03_caz2_fulldata/features.txt',
+  columns=c('filename'),
+  all_selected_features=TRUE
+)
+
+# Exercise 4
+create_features_file(
+  features_file='../data/current_sdm_main.csv',
+  output_file='../../04_caz2_fulldata_wghts/features.txt',
+  columns=c('weight', 'filename'),
+  all_selected_features=TRUE
+)
+
+# Exercise 5
+create_features_file(
+  features_file='../data/current_sdm_main.csv',
+  output_file='../../05_caz2_fulldata_wghts_humanfootprint/features.txt',
+  columns=c('weight', 'filename'),
+  all_selected_features=TRUE
+)
+write(
+  '-10 "../data/other_layers/human_footprint2018.tif"',
+    '../../05_caz2_fulldata_wghts_humanfootprint/features.txt',
+  append=TRUE
+)
+
+# Exercise 6 natura2000
+create_features_file(
+  features_file='../data/current_sdm_main.csv',
+  output_file='../../06_caz2_fulldata_wghts_hmask_natura2000/features.txt',
+  columns=c('weight', 'filename'),
+  all_selected_features=TRUE
+)
+
+# Exercise 6 wpda
+create_features_file(
+  features_file='../data/current_sdm_main.csv',
+  output_file='../../06_caz2_fulldata_wghts_hmask_wdpa/features.txt',
+  columns=c('weight', 'filename'),
+  all_selected_features=TRUE
+)
