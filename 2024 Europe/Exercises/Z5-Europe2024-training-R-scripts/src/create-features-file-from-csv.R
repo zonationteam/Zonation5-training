35cdaf756917baf8bdbb0951f8a42c3f8aa6bbd1
kivistoilkka <ilkka.kivisto@gmail.com>
2024-05-30 19:12:10 +0300

    Update new file paths to R script, create features.txt for extra tasks

--- 2024 Europe/Exercises/Z5-Europe2024-training-R-scripts/src/create-features-file-from-csv.R
+++ 2024 Europe/Exercises/Z5-Europe2024-training-R-scripts/src/create-features-file-from-csv.R
@@ -10,9 +10,18 @@ create_features_file <- function(
   features_file,
   output_file,
   columns,
+  all_features=FALSE,
   all_selected_features=FALSE
 ) {
   features_df <- read.delim(features_file, sep=' ')
+  if (all_features) {
+    write.table(
+      features_df %>% select(all_of(columns)),
+      output_file,
+      row.names=FALSE
+    )
+    return (0)
+  }
   output_df <- features_df %>%
     filter(SELECTED != '0') %>%
     arrange(SELECTED)
@@ -41,7 +50,7 @@ create_features_file <- function(
 # Exercise 1
 create_features_file(
   features_file='../data/current_sdm_main.csv',
-  output_file='../../01_caz2/features.txt',
+  output_file='../../01_caz2_hw/features.txt',
   columns=c('filename'),
   all_selected_features=FALSE
 )
@@ -49,7 +58,7 @@ create_features_file(
 # Exercise 2
 create_features_file(
   features_file='../data/current_sdm_main.csv',
-  output_file='../../02_cazmax/features.txt',
+  output_file='../../02_cazmax_hw/features.txt',
   columns=c('filename'),
   all_selected_features=FALSE
 )
@@ -57,7 +66,7 @@ create_features_file(
 # Exercise 3
 create_features_file(
   features_file='../data/current_sdm_main.csv',
-  output_file='../../03_caz2_fulldata/features.txt',
+  output_file='../../03_caz2_hw_sw/features.txt',
   columns=c('filename'),
   all_selected_features=TRUE
 )
@@ -65,7 +74,7 @@ create_features_file(
 # Exercise 4
 create_features_file(
   features_file='../data/current_sdm_main.csv',
-  output_file='../../04_caz2_fulldata_wghts/features.txt',
+  output_file='../../04_caz2_hw_sw_wghts/features.txt',
   columns=c('weight', 'filename'),
   all_selected_features=TRUE
 )
@@ -73,20 +82,28 @@ create_features_file(
 # Exercise 5
 create_features_file(
   features_file='../data/current_sdm_main.csv',
-  output_file='../../05_caz2_fulldata_wghts_gHM/features.txt',
+  output_file='../../05_caz2_hw_sw_wghts_gHM/features.txt',
   columns=c('weight', 'filename'),
   all_selected_features=TRUE
 )
 write(
   '-10 "../data/other_layers/gHM_Europe.tif"',
-    '../../05_caz2_fulldata_wghts_gHM/features.txt',
+    '../../05_caz2_hw_sw_wghts_gHM/features.txt',
   append=TRUE
 )
 
 # Exercise 6
 create_features_file(
   features_file='../data/current_sdm_main.csv',
-  output_file='../../06_caz2_fulldata_wghts_hmask/features.txt',
+  output_file='../../06_caz2_hw_sw_wghts_hmask/features.txt',
   columns=c('weight', 'filename'),
   all_selected_features=TRUE
+)
+
+# Extra tasks
+create_features_file(
+  features_file='../data/current_sdm_main.csv',
+  output_file='../../00_extra_tasks/features.txt',
+  columns=c('weight', 'filename'),
+  all_features=TRUE
 )
